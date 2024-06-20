90d72b5786aabeff856ef43b32aa3557121da2df
Vitalii Shapovalov <martyit@ukr.net>
2024-06-20 15:20:28 +0200

    styling components

--- src/app/components/movie-sidebar/movie-sidebar.component.ts
+++ src/app/components/movie-sidebar/movie-sidebar.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
-import { MenubarModule } from 'primeng/menubar';
 import { SidebarModule } from 'primeng/sidebar';
 
 @Component({
   selector: 'app-movie-sidebar',
   standalone: true,
-  imports: [MenubarModule, ButtonModule, SidebarModule],
+  imports: [ButtonModule, SidebarModule],
   templateUrl: './movie-sidebar.component.html',
   styleUrl: './movie-sidebar.component.scss'
 })
