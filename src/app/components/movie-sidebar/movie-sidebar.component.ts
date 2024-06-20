0b28ec99e73d4dd426250d8ee8f061bfef2fd03d
Vitalii Shapovalov <martyit@ukr.net>
2024-06-20 21:41:07 +0200

    visual part made

--- src/app/components/movie-sidebar/movie-sidebar.component.ts
+++ src/app/components/movie-sidebar/movie-sidebar.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { SidebarModule } from 'primeng/sidebar';
+import { MovieAsideMenuComponent } from "../movie-aside-menu/movie-aside-menu.component";
 
 @Component({
-  selector: 'app-movie-sidebar',
-  standalone: true,
-  imports: [ButtonModule, SidebarModule],
-  templateUrl: './movie-sidebar.component.html',
-  styleUrl: './movie-sidebar.component.scss'
+    selector: 'app-movie-sidebar',
+    standalone: true,
+    templateUrl: './movie-sidebar.component.html',
+    styleUrl: './movie-sidebar.component.scss',
+    imports: [ButtonModule, SidebarModule, MovieAsideMenuComponent]
 })
 export class MovieSidebarComponent {
 	sidebarVisible: boolean = false
