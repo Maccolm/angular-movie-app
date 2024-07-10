7d2d77fd791a94467f58e7c4d6a8196d6ea4cc2d
Vitalii Shapovalov <martyit@ukr.net>
2024-07-10 21:50:32 +0200

    Cleaning code, making that shorter

--- src/app/components/aside-menu/aside-menu.component.ts
+++ src/app/components/aside-menu/aside-menu.component.ts
@@ -6,8 +6,33 @@ import { RouterModule } from '@angular/router';
   standalone: true,
   imports: [RouterModule],
   templateUrl: './aside-menu.component.html',
-  styleUrl: './aside-menu.component.scss'
+  styleUrl: './aside-menu.component.scss',
 })
 export class MovieAsideMenuComponent {
-
+  links = [
+    {
+      name: 'Now playing',
+      url: 'now-playing',
+      icon: 'pi pi-play-circle',
+      color: 'color: rgb(89, 133, 92)',
+    },
+    {
+      name: 'Popular',
+      url: 'popular',
+      icon: 'pi pi-chart-line',
+      color: 'color: rgb(236, 200, 90)',
+    },
+    {
+      name: 'Top rate',
+      url: 'top-rate',
+      icon: 'pi pi-star-fill',
+      color: 'color: rgb(255, 140, 68)',
+    },
+    {
+      name: 'Upcoming',
+      url: 'upcoming',
+      icon: 'pi pi-calendar-clock',
+      color: 'color: rgb(89, 110, 133)',
+    },
+  ];
 }
--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -5,12 +5,25 @@ import { MovieAsideMenuComponent } from "../aside-menu/aside-menu.component";
 import { RouterLink, RouterModule } from '@angular/router';
 
 @Component({
-    selector: 'app-movie-sidebar',
-    standalone: true,
-    templateUrl: './sidebar.component.html',
-    styleUrl: './sidebar.component.scss',
-    imports: [ButtonModule, SidebarModule, MovieAsideMenuComponent, RouterLink, RouterModule]
+  selector: 'app-movie-sidebar',
+  standalone: true,
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.scss',
+  imports: [
+    ButtonModule,
+    SidebarModule,
+    MovieAsideMenuComponent,
+    RouterLink,
+    RouterModule,
+  ],
 })
 export class MovieSidebarComponent {
-	sidebarVisible: boolean = false
+  sidebarVisible: boolean = false;
+
+  links = [
+    { name: 'Now playing', url: 'now-playing' },
+    { name: 'Popular', url: 'popular' },
+    { name: 'Top Rate', url: 'top-rate' },
+    { name: 'Upcoming', url: 'upcoming' },
+  ];
 }
