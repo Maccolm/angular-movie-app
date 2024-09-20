ba9b5f01745fcde793da52afb24591a8542c4b20
Vitalii Shapovalov <martyit@ukr.net>
2024-09-20 14:37:23 +0200

    have written tests on sidebar component

--- src/app/components/sidebar/sidebar.component.spec.ts
+++ src/app/components/sidebar/sidebar.component.spec.ts
@@ -22,4 +22,73 @@ describe('MovieSidebarComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('onDocumentClick', () => {
+	it('should close sidebar if clicked outside the sidebar and other specific elements', () => {
+		component.sidebarVisible = true;
+		const event = new MouseEvent('click', {
+			bubbles: true,
+			cancelable: true,
+			view: window
+		});
+		const target = document.createElement('div');
+		document.body.appendChild(target);
+		Object.defineProperty(event, 'target', { value: target });
+		component.onDocumentClick(event as any);
+		expect(component.sidebarVisible).toBe(false);
+		document.body.removeChild(target);
+	});
+
+	it('should not close sidebar if clicked inside the sidebar', () => {
+		component.sidebarVisible = true;
+
+		const sidebarElement = document.createElement('div');
+		sidebarElement.classList.add('custom-sidebar');
+		document.body.appendChild(sidebarElement);
+
+		const event = new MouseEvent('click', {
+			bubbles: true,
+			cancelable: true,
+			view: window
+		});
+		Object.defineProperty(event, 'target', { value: sidebarElement });
+		component.onDocumentClick(event as any);
+		expect(component.sidebarVisible).toBe(true);
+		document.body.removeChild(sidebarElement);
+	});
+
+	it('should not close sidebar if clicked on the bar button', () => {
+		component.sidebarVisible = true;
+		const buttonElement = document.createElement('button');
+		buttonElement.classList.add('bar-button');
+		document.body.append(buttonElement);
+
+		const event = new MouseEvent('click', {
+			bubbles: true,
+			cancelable: true,
+			view: window
+		});
+		Object.defineProperty(event, 'target', { value: buttonElement });
+		component.onDocumentClick(event as any);
+		expect(component.sidebarVisible).toBe(true);
+		document.body.removeChild(buttonElement);
+	});
+	it('should close sidebar with delay if clicked on the submit filter button', () => {
+		jest.useFakeTimers();
+		component.sidebarVisible = true;
+		const submitButton = document.createElement('button');
+		submitButton.classList.add('filter__button-submit');
+		document.body.appendChild(submitButton);
+
+		const event = {
+			target: submitButton,
+			stopPropagation: jest.fn(),
+			preventDefault: jest.fn()
+		} as unknown as MouseEvent;
+		component.onDocumentClick(event);
+		jest.advanceTimersByTime(300);
+		expect(component.sidebarVisible).toBe(false);
+		document.body.removeChild(submitButton);
+	});
+	});
 });
